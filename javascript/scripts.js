046c6a83bd6ace6dbe8c02187bd500863bb888b6
Vladyslav <valdrudik@gmail.com>
2020-09-22 12:07:49 +0300

    test

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -30,37 +30,67 @@ $(document).ready(function(){
 
 $('#submit_btn').on('click', sendForm2);
 function sendForm2() {
+    $('.text-error').slideUp();
+    $('.text-success').slideUp();
     let name = $('#name').val();
     let email = $('#email').val();
     let request = $('#request').val();
+    var reg = /^\w+([\.-]?\w+)*@(((([a-z0-9]{2,})|([a-z0-9][-][a-z0-9]+))[\.][a-z0-9])|([a-z0-9]+[-]?))+[a-z0-9]+\.([a-z]{2}|(com|net|org|edu|int|mil|gov|arpa|biz|aero|name|coop|info|pro|museum))$/i;
+    var v_name = name?false:true;
+    var v_email = email?false:true;
+    var v_request = request?false:true;
     const token = "1313923746:AAG03pSHtn9pgiWvmJC0FG3Eo-TKG2IPFXs";
     const chatId = "-436967359";
     let url = 'https://api.telegram.org/bot' + token +'/sendMessage?chat_id=' + chatId + '&text=';
-
     let xhttp = new XMLHttpRequest();
-
-    xhttp.open("GET", url + "Hello Yaroslav, you have a new client.%0AName : " + name + ",%0AEmail : " + email + ",%0ARequest : " + request, true);
-    xhttp.send();
-    $('#name').val("");
-    $('#email').val("");
-    $('#request').val("");
+    if(v_name && v_email && v_request)
+    {
+        $('.text-error').slideDown();
+    } 
+    else if(!reg.test($('#email').val()))
+    {
+        $('.text-error').slideDown();
+    }
+    else{
+        $('.text-success').slideDown();
+        xhttp.open("GET", url + "Hello Yaroslav, you have a new client.%0AName : " + name + ",%0AEmail : " + email + ",%0ARequest : " + request, true);
+        xhttp.send();
+        $('#name').val("");
+        $('#email').val("");
+        $('#request').val("");
+    }
 };
 
 
 $('#submit_btn2').on('click', sendForm);
 function sendForm() {
+    $('.text-error2').slideUp();
+    $('.text-success2').slideUp();
     let name = $('#name2').val();
     let email = $('#email2').val();
     let request = $('#request2').val();
+    var reg = /^\w+([\.-]?\w+)*@(((([a-z0-9]{2,})|([a-z0-9][-][a-z0-9]+))[\.][a-z0-9])|([a-z0-9]+[-]?))+[a-z0-9]+\.([a-z]{2}|(com|net|org|edu|int|mil|gov|arpa|biz|aero|name|coop|info|pro|museum))$/i;
+    var v_name = name?false:true;
+    var v_email = email?false:true;
+    var v_request = request?false:true;
     const token = "1313923746:AAG03pSHtn9pgiWvmJC0FG3Eo-TKG2IPFXs";
     const chatId = "-436967359";
     let url = 'https://api.telegram.org/bot' + token +'/sendMessage?chat_id=' + chatId + '&text=';
-
     let xhttp = new XMLHttpRequest();
-
-    xhttp.open("GET", url + "Hello Yaroslav, you have a new client.%0AName : " + name + ",%0AEmail : " + email + ",%0ARequest : " + request, true);
-    xhttp.send();
-    $('#name2').val("");
-    $('#email2').val("");
-    $('#request2').val("");
+    if(v_name && v_email && v_request)
+    {
+        $('.text-error2').slideDown();
+    } 
+    else if(!reg.test($('#email2').val()))
+    {
+        $('.text-error2').slideDown();
+    }
+    else{
+        $('.text-success2').slideDown();
+        xhttp.open("GET", url + "Hello Yaroslav, you have a new client.%0AName : " + name + ",%0AEmail : " + email + ",%0ARequest : " + request, true);
+        xhttp.send();
+        $('#name2').val("");
+        $('#email2').val("");
+        $('#request2').val("");
+    }
 };
